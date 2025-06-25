08ef278feee234b4e9dfb50048550b4036fb2b9b
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-06-25 17:56:07 -0300

    produtos

--- src/pages/Chairs.jsx
+++ src/pages/Chairs.jsx
@@ -1,7 +1,54 @@
+import SearchProducts from "../utils/SearchProducts";
 
 const Chairs = () => {
+     const productChairs = SearchProducts("chair");
   return (
-    <div>Chairs</div>
+    <section className="section">
+          <div className="content productSection">
+            <h1>Lançamentos</h1>
+            <h4>Conheça os nossos principais produtos!</h4>
+            <div className="products">
+              <div className="product">
+                <img src={productChairs[0].imageUrl} alt={productChairs[0].name} />
+                <p className="name">{productChairs[0].name}</p>
+                <p className="description">{productChairs[0].description}</p>
+                <p className="price"><span>R$</span>{productChairs[0].price}</p>
+              </div>
+              <div className="product">
+                <img src={productChairs[1].imageUrl} alt={productChairs[1].name} />
+                <p className="name">{productChairs[1].name}</p>
+                <p className="description">{productChairs[1].description}</p>
+                <p className="price"><span>R$</span>{productChairs[1].price}</p>
+              </div>
+    
+              <div className="product">
+                <img src={productChairs[0].imageUrl} alt={productChairs[0].name} />
+                <p className="name">{productChairs[0].name}</p>
+                <p className="description">{productChairs[0].description}</p>
+                <p className="price"><span>R$</span>{productChairs[0].price}</p>
+              </div>
+              <div className="product">
+                <img src={productChairs[1].imageUrl} alt={productChairs[1].name} />
+                <p className="name">{productChairs[1].name}</p>
+                <p className="description">{productChairs[1].description}</p>
+                <p className="price"><span>R$</span>{productChairs[1].price}</p>
+              </div>
+    
+              <div className="product">
+                <img src={productChairs[0].imageUrl} alt={productChairs[0].name} />
+                <p className="name">{productChairs[0].name}</p>
+                <p className="description">{productChairs[0].description}</p>
+                <p className="price"><span>R$</span>{productChairs[0].price}</p>
+              </div>
+              <div className="product">
+                <img src={productChairs[1].imageUrl} alt={productChairs[1].name} />
+                <p className="name">{productChairs[1].name}</p>
+                <p className="description">{productChairs[1].description}</p>
+                <p className="price"><span>R$</span>{productChairs[1].price}</p>
+              </div>
+            </div>
+          </div>
+        </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
    const productMiscellaneous = SearchProducts("miscellaneous");
   return (
     <section className="section">
-      <div className="content homePage">
+      <div className="content productSection">
         <h1>Lançamentos</h1>
         <h4>Conheça os nossos principais produtos!</h4>
         <div className="products">
@@ -18,42 +18,42 @@ const Home = () => {
             <img src={productTables[0].imageUrl} alt={productTables[0].name} />
             <p className="name">{productTables[0].name}</p>
             <p className="description">{productTables[0].description}</p>
-            <p className="price">{productTables[0].price}</p>
+            <p className="price"><span>R$</span>{productTables[0].price}</p>
           </div>
           <div className="product">
             <img src={productTables[1].imageUrl} alt={productTables[1].name} />
             <p className="name">{productTables[1].name}</p>
             <p className="description">{productTables[1].description}</p>
-            <p className="price">{productTables[1].price}</p>
+            <p className="price"><span>R$</span>{productTables[1].price}</p>
           </div>
 
           <div className="product">
             <img src={productChairs[0].imageUrl} alt={productChairs[0].name} />
             <p className="name">{productChairs[0].name}</p>
             <p className="description">{productChairs[0].description}</p>
-            <p className="price">{productChairs[0].price}</p>
+            <p className="price"><span>R$</span>{productChairs[0].price}</p>
           </div>
           <div className="product">
             <img src={productChairs[1].imageUrl} alt={productChairs[1].name} />
             <p className="name">{productChairs[1].name}</p>
             <p className="description">{productChairs[1].description}</p>
-            <p className="price">{productChairs[1].price}</p>
+            <p className="price"><span>R$</span>{productChairs[1].price}</p>
           </div>
 
           <div className="product">
             <img src={productMiscellaneous[0].imageUrl} alt={productMiscellaneous[0].name} />
             <p className="name">{productMiscellaneous[0].name}</p>
             <p className="description">{productMiscellaneous[0].description}</p>
-            <p className="price">{productMiscellaneous[0].price}</p>
+            <p className="price"><span>R$</span>{productMiscellaneous[0].price}</p>
           </div>
           <div className="product">
             <img src={productMiscellaneous[1].imageUrl} alt={productMiscellaneous[1].name} />
             <p className="name">{productMiscellaneous[1].name}</p>
             <p className="description">{productMiscellaneous[1].description}</p>
-            <p className="price">{productMiscellaneous[1].price}</p>
+            <p className="price"><span>R$</span>{productMiscellaneous[1].price}</p>
           </div>
         </div>
-        <button className="bnt">
+        <button className="btn">
           <Link to="/orders">Fazer Pedido</Link>
         </button>
       </div>
