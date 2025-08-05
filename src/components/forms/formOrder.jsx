bd429188e46e1397f8608cd56609bc79dd73218a
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-08-05 18:19:59 -0300

    hook citiesMeta

--- src/components/forms/formOrder.jsx
+++ src/components/forms/formOrder.jsx
@@ -1,9 +1,9 @@
 import { useForm } from "react-hook-form";
 import SearchProducts from "../../utils/SearchProducts";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import "./styles/order.css";
 import { ErrorMessage } from "../Error";
-import axios from "axios";
+import { Location } from "./hooks/citiesMeta";
 
 const Form = () => {
 
@@ -102,35 +102,6 @@ const Form = () => {
     clean();
   };
 
-  const [ufs, setUfs]= useState([])
-  const [cities, setCities]= useState([])
-  // const [selectedUf, setSelectedUf] = useState("")
-  const ufWatch = watch("state")
-
-  useEffect(()=> {
-    axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/').then((response) => {
-      setUfs(response.data);
-    })
-  }, [])
-
-  useEffect(()=> { 
-    if (ufWatch) {
-      axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufWatch}/municipios`).then((response) => {
-        setCities(response.data);
-      })
-    } else{
-      setCities([])
-    } 
-  }, [ufWatch])
-
-
-
-
-  // const handleSelectedUf = (event) => {
-  //   const uf = event.target.value
-  //   setSelectedUf(uf)
-  // }
-
   return (
     <div className="form">
       <label htmlFor="name">Nome do Vendedor:</label>
@@ -175,33 +146,7 @@ const Form = () => {
           />
           <ErrorMessage error={errors.adress} fieldName="Endereço" />
         </div>
-        <div className="uf">
-          <label className="state" htmlFor="state">
-            UF:
-          </label>
-          <select
-            id="state"
-            name="state"
-            placeholder="UF"
-            {...register("state", { required: true })}
-          >
-            <option value="">Selecione a UF</option>
-            {ufs.map(uf => (
-              <option key={uf.id} value={uf.sigla}>{uf.nome}</option>
-            ))}
-          </select>
-          <ErrorMessage error={errors.state} fieldName="Estado" />
-        </div>
-        <div>
-          <label htmlFor="city">Cidade:</label>
-          <select name="city" id="city" {...register("city", { required: true })}>
-            <option value="0">Selecione a cidade</option>
-            {cities.map((city) => (
-              <option key={city.id} value={city.nome}>{city.nome}</option>
-            ))}
-          </select>
-          <ErrorMessage error={errors.city} fieldName="Cidade" />
-        </div>
+        <Location watch={watch} errors={errors} register={register} />
         <div>
           <label htmlFor="phone">Telefone:</label>
           <input
---
+++ src/components/forms/hooks/citiesMeta.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ErrorMessage } from "../../Error";
+
+const Location = ({ watch, errors, register }) => {
+  const [ufs, setUfs] = useState([]);
+  const [cities, setCities] = useState([]);
+  const ufWatch = watch("state");
+
+  useEffect(() => {
+    axios
+      .get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/")
+      .then((response) => {
+        setUfs(response.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (ufWatch) {
+      axios
+        .get(
+          `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufWatch}/municipios`
+        )
+        .then((response) => {
+          setCities(response.data);
+        });
+    } else {
+      setCities([]);
+    }
+  }, [ufWatch]);
+
+  return (
+    <>
+      <div className="uf">
+        <label className="state" htmlFor="state">
+          UF:
+        </label>
+        <select
+          id="state"
+          name="state"
+          placeholder="UF"
+          {...register("state", { required: true })}
+        >
+          <option value="">Selecione a UF</option>
+          {ufs.map((uf) => (
+            <option key={uf.id} value={uf.sigla}>
+              {uf.nome}
+            </option>
+          ))}
+        </select>
+        <ErrorMessage error={errors.state} fieldName="Estado" />
+      </div>
+      <div>
+        <label htmlFor="city">Cidade:</label>
+        <select name="city" id="city" {...register("city", { required: true })}>
+          <option value="0">Selecione a cidade</option>
+          {cities.map((city) => (
+            <option key={city.id} value={city.nome}>
+              {city.nome}
+            </option>
+          ))}
+        </select>
+        <ErrorMessage error={errors.city} fieldName="Cidade" />
+      </div>
+    </>
+  );
+};
+
+export { Location };
